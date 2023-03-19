e9f1fb33fae9af3f5d0ec0e78ac758015d57cc5f
Vinicius Koiti <viniciusnakahara@gmail.com>
2023-03-19 16:24:28 -0300

    Criando Tabela pelo css e separadando dados

--- index.js
+++ index.js
@@ -1,52 +1,94 @@
 const form = document.querySelector("form");
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    const urlDoGithub = document.querySelector("#repositorio").value;
-    const dataInicial = document.querySelector("#dataInicial").value;
-    const dataFinal = document.querySelector("#dataFinal").value;
-    const repositorio = trataUrl(urlDoGithub);
-    //console.log(repositorio + " " +dataInicial+" "+dataFinal);
+    // const urlDoGithub = document.querySelector("#repositorio").value;
+    // const dataInicial = document.querySelector("#dataInicial").value;
+    // const dataFinal = document.querySelector("#dataFinal").value;
+    // const repositorio = trataUrl(urlDoGithub);
+
+    const dataInicial = "2019-01-01";
+    const dataFinal = "2023-01-01";
+    const repositorio = trataUrl("https://github.com/frankwco/loja");
+    console.log("teste");
     buscarCommits(repositorio, dataInicial, dataFinal);
 });
-
-let mockUrl = "https://github.com/frankwco/loja";
 
 function buscarCommits(repositorio, dataInicial, dataFinal){
     const url = `https://api.github.com/repos/${repositorio}/commits?since=${dataInicial}&until=${dataFinal}`
     fetch(url).then(response => response.json()).then(
-        commits => contarCommits(commits)
+        commits => preparaDados(commits)
                 
             );    
         console.log(new Date());
 }
 
-function contarCommits(commits){
+
+function preparaDados(commits){
     const commitsPorDia = {};
     commits.forEach(element => {
         const dataCommit = element.commit.author.date.substr(0,10);
+        const authorCommit = element.commit.author.name;
+        const messageCommit = element.commit.message;
         if(commitsPorDia[dataCommit]){
             commitsPorDia[dataCommit].quantidade++;
         }else{
-            commitsPorDia[dataCommit] = {quantidade:1, data:dataCommit};
+            commitsPorDia[dataCommit] = {quantidade:1, data:dataCommit,author:authorCommit,message:messageCommit};
         }
     });
     console.log(commitsPorDia);
     
     const commitsPordiaArray = Object.keys(commitsPorDia).map(dataCommit=>{
-        return {data:dataCommit, quantidade:commitsPorDia[dataCommit].quantidade};
+        return {data:dataCommit, quantidade:commitsPorDia[dataCommit].quantidade,author:commitsPorDia[dataCommit].author,message:commitsPorDia[dataCommit].message};
     })
 
 
-    mostrarTela(commitsPordiaArray);
+    criaTabela(commitsPordiaArray);
 } 
-function mostrarTela(commits){
-    const dados= document.querySelector("#dados");
-    commits.forEach(element => {
-        const h1 = document.createElement("h1");
-        h1.innerHTML = element.data +" - "+element.quantidade;
-        dados.appendChild(h1);
-    })
-}
+function criaTabela(dados) {
+    const tabela = document.querySelector("#tabela-dados");
+    
+    // Cria as colunas da tabela
+    const colunaData = document.createElement("td");
+    const colunaQuantidade = document.createElement("td");
+    const colunaAuthor = document.createElement("td");
+    const colunaMessagem = document.createElement("td");
+    // Adiciona o conteúdo das colunas do cabeçalho
+    colunaData.textContent = "Data";
+    colunaQuantidade.textContent = "Quantidade";
+    colunaAuthor.textContent = "Autor";
+    colunaMessagem.textContent = "Messagem";
+  
+    // Adiciona as colunas ao cabeçalho da tabela
+    const cabecalho = document.createElement("tr");
+    cabecalho.appendChild(colunaData);
+    cabecalho.appendChild(colunaQuantidade);
+    cabecalho.appendChild(colunaAuthor);
+    cabecalho.appendChild(colunaMessagem);
+    tabela.appendChild(cabecalho);
+  
+    // Adiciona as linhas com os dados na tabela
+    dados.forEach((commit) => {
+      const linha = document.createElement("tr");
+  
+      const celulaData = document.createElement("td");
+      celulaData.textContent = commit.data;
+      linha.appendChild(celulaData);
+  
+      const celulaQuantidade = document.createElement("td");
+      celulaQuantidade.textContent = commit.quantidade;
+      linha.appendChild(celulaQuantidade);
+  
+      const colunaAuthor = document.createElement("td");
+      colunaAuthor.textContent = commit.author; // Substitua "outroDado" pelo nome do outro dado que você deseja adicionar à tabela
+      linha.appendChild(colunaAuthor);
+
+      const colunaMessagem = document.createElement("td");
+      colunaMessagem.textContent = commit.message;
+      linha.appendChild(colunaMessagem);
+  
+      tabela.appendChild(linha);
+    });
+  }
 
 function trataUrl(url){
 
@@ -59,5 +101,3 @@ function trataUrl(url){
     
     return usuarioGithub + "/" + repositorioGithub;
 }
-
-console.log(trataUrl(mockUrl));
